5ff847c920db6609d11ceba4690ad45de96810e1
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-10-21 22:35:29 +0300

    reuse react component

--- Movie-List/src/App.jsx
+++ Movie-List/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import MovieList from './components/MovieList'
+
 
 function App() {
+	const movies = [
+		'The matrix',
+		'The Man of Steel',
+		'Lord of the rings',
+		'Case for Christ'
+	];
 
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<>
+			<MovieList movies={movies} />
+		</>
+	)
 }
 
 export default App
