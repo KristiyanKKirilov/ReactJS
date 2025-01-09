2f867f50467ede552f956980a7e8a00153ade334
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-01-09 19:14:17 +0200

    uncontrolled form eg.

---
+++ 05.Forms/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UnControlledForm from './components/UnControlledForm'
+
+function App() {
+
+  return (
+    <>
+    <UnControlledForm/>
+    </>
+  )
+}
+
+export default App
