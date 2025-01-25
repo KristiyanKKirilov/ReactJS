9844274d2bfb74452d0055b637d5a68491a47c98
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-01-25 14:10:44 +0200

    header component refactored

--- 07.Router-Workshop/client/src/App.jsx
+++ 07.Router-Workshop/client/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/header/Header";
+
 function App() {
   return (
-     <>
-        <h1>Working</h1>
-     </>
+     <div id="box">
+        <Header/>
+     </div>
   );
 }
 
