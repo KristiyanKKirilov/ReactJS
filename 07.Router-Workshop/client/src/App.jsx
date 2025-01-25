053f4f2e2051050e29a2ef28e134ae3cf269175f
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-01-25 14:13:39 +0200

    home component refactored

--- 07.Router-Workshop/client/src/App.jsx
+++ 07.Router-Workshop/client/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
   return (
      <div id="box">
         <Header/>
+        <main id="main-content">
+            <Home/>
+        </main>
      </div>
   );
 }
