46047a25490bb1dfe3262937147825ff6befbc3d
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-01-27 15:43:02 +0200

    page routes added

--- 07.Router-Workshop/client/src/App.jsx
+++ 07.Router-Workshop/client/src/App.jsx
@@ -1,12 +1,26 @@
+import { Routes, Route} from 'react-router-dom';
+
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Login from "./components/authentication/login/Login";
+import Register from "./components/authentication/register/Register";
+import GameCatalog from './components/game/game-catalog/GameCatalog';
+import GameCreate from './components/game/game-create/GameCreate';
+import GameDetails from './components/game/game-details/GameDetails';
+
 
 function App() {
   return (
      <div id="box">
         <Header/>
         <main id="main-content">
-            <Home/>
+            <Routes>
+               <Route path="/" element={<Home/>}/>
+               <Route path="/login" element={<Login/>}/>
+               <Route path="/register" element={<Register/>}/>
+               <Route path="/games" element={<GameCatalog/>}/>
+               <Route path="/games/create" element={<GameCreate/>}/>
+            </Routes>
         </main>
      </div>
   );
