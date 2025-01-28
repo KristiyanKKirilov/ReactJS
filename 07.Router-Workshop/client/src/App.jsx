ee723f837e9e2d88fe612f45c826daf1c95bfb69
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-01-28 19:46:53 +0200

    router to details component

--- 07.Router-Workshop/client/src/App.jsx
+++ 07.Router-Workshop/client/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
                <Route path="/register" element={<Register/>}/>
                <Route path="/games" element={<GameCatalog/>}/>
                <Route path="/games/create" element={<GameCreate/>}/>
+               <Route path="/games/:gameId" element={<GameDetails/>}/>
             </Routes>
         </main>
      </div>
