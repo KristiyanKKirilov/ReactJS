74c4b4825621698dcd371c45ff2f0083d45d7b4a
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-02-03 17:56:29 +0200

    spinner added until data is fully fetched

--- 08.Hooks/client/src/App.jsx
+++ 08.Hooks/client/src/App.jsx
@@ -3,18 +3,28 @@ import { BASE_URL } from "./constants";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import ArticleCard from "./components/article-card/ArticleCard";
+import Button from 'react-bootstrap/Button';
 import NavBar from "./components/nav-bar/NavBar";
-import styles from './App.module.css';
+import styles from "./App.module.css";
+import LoadingSpinner from "./components/loading-spinner/LoadingSpinner";
 
 function App() {
-    const {data: articles} = useFetch(BASE_URL, []);
+  const { data: articles, isFetching, refetch } = useFetch(BASE_URL, []);
 
   return (
     <>
       <NavBar />
-      <div className={styles["article-list"]}>
-        {articles.map(article => <ArticleCard key={article._id} {...article}/>)}
-      </div>
+      {!isFetching ? (
+        <div className={styles["article-list"]}>
+          {articles.map((article) => (
+            <ArticleCard key={article._id} {...article} />
+          ))}
+        </div>
+      ) : (
+        <LoadingSpinner />
+      )}
+
+        <Button variant="dark" onClick={refetch}>Refetch</Button>
     </>
   );
 }
