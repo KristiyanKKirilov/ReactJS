be23e9e4287130e68cb17fee768f35d770ba5899
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-02-03 17:11:27 +0200

    using dynaimic data, modified server collections

--- 08.Hooks/client/src/components/article-card/ArticleCard.jsx
+++ 08.Hooks/client/src/components/article-card/ArticleCard.jsx
@@ -3,18 +3,19 @@ import Card from 'react-bootstrap/Card';
 
 export default function ArticleCard({
     _id,
-    title
+    title,
+    imageUrl
 }) {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://media.istockphoto.com/id/1177502660/photo/young-woman-reading-the-news-on-a-modern-tablet-computer-while-sitting-in-her-living-room.jpg?s=612x612&w=0&k=20&c=oEfXfMaKkgAVfshd7yk_bxGk2iQncWueLVlTL__gWWg=" />
+      <Card.Img variant="top" src={imageUrl} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>
+        {/* <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.   
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        </Card.Text> */}
+        <Button variant="danger">Details</Button>
       </Card.Body>
     </Card>
   );
