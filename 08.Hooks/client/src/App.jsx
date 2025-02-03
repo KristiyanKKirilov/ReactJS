64e08f2bccd646dc5f73b60b1fd9b1cb153fae9b
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-02-03 17:03:49 +0200

    get request to servercreated, fetching articles

--- 08.Hooks/client/src/App.jsx
+++ 08.Hooks/client/src/App.jsx
@@ -2,16 +2,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ArticleCard from "./components/article-card/ArticleCard";
 import NavBar from "./components/nav-bar/NavBar";
 import styles from './App.module.css';
+import { useEffect, useState } from "react";
+import { BASE_URL } from "./constants";
 
 function App() {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(BASE_URL);
+            const result = await response.json();
+            setArticles(result);
+        })();
+    }, []);
+
   return (
     <>
       <NavBar />
       <div className={styles["article-list"]}>
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
+        {articles.map(article => <ArticleCard key={article._id} {...article}/>)}
       </div>
     </>
   );
