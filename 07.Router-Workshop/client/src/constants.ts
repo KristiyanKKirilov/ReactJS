c273aca370e7026f4845b3f7dc8c86d65d2d6d25
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-02-09 03:53:08 +0200

    urls fixed

--- 07.Router-Workshop/client/src/constants.ts
+++ 07.Router-Workshop/client/src/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = "http://localhost:3030/data/games";
-export const CREATE_BASE_URL = "http://localhost:3030/data/games";
 export const AUTH_BASE_URL = "http://localhost:3030/users";
+export const LATEST_GAMES_URL = "http://localhost:3030/data/games?sortBy=_createdOn%20desc&pageSize=3";
  
